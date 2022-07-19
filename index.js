25f3a8b4af362c9e80100437e5c7b0c5197822e5
jumpbangs <amitchongbang@gmail.com>
2022-07-19 13:10:54 +0545

    Add slient push notification endpoint

--- index.js
+++ index.js
@@ -13,28 +13,38 @@ admin.initializeApp({
 const APP = express();
 
 const fcmMessage = {
-  //   apns: {
-  //     payload: {
-  //       aps: {
-  //         "content-available": 1,
-  //         alert: "",
-  //       },
-  //     },
-  //   },
-  //   notification: {
-  //     title: "Firebase Notification",
-  //     body: "Testing for Firebase Notification",
-  //   },
+  // notification: {
+  //   body: "Firebase Cloud Message Body",
+  //   title: "Firebase Cloud Message Title",
+  // },
   data: {
+    urlLink: "",
     notificationType: "local",
     liveStreamReminderTime: "12345567",
-    key3: "one_more",
   },
-  token:
-    "cAfOVkfLRbSBauvLXr88wB:APA91bGN26BeXFvgdSu6SVGZUzflzlDaCtv1cGfYDG_hxACnn6zp-AkFoerA7doRc9i78d30clzY8yt16qvlmQUrPH2c74Vs83YoyHoiG4J32P3JKpw3vfn_FIwd3oMuk8Sp8jfWAyKW",
+  topic: "push-notification-reminder",
+  // token:
+  //   "cAfOVkfLRbSBauvLXr88wB:APA91bGN26BeXFvgdSu6SVGZUzflzlDaCtv1cGfYDG_hxACnn6zp-AkFoerA7doRc9i78d30clzY8yt16qvlmQUrPH2c74Vs83YoyHoiG4J32P3JKpw3vfn_FIwd3oMuk8Sp8jfWAyKW",
 };
 
-const sendPushNotification = async (message) => {
+const sendPushNotification = async (type, reminderTime) => {
+  const fcmMessage = {
+    data: {
+      notificationType: type,
+      liveStreamReminderTime: reminderTime,
+    },
+    topic: "Live stream notification",
+  };
+
+  try {
+    const response = await admin.messaging().send(fcmMessage);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const sendSilentPushNotification = async (message) => {
   try {
     const response = await admin.messaging().send(message);
     console.log(response);
@@ -45,9 +55,19 @@ const sendPushNotification = async (message) => {
 
 APP.use(cors());
 
-APP.get("/", (req, res) => {
+APP.post("/push", (req, res) => {
+  const type = req.query?.type;
+  const reminderTime = req.query?.reminderTime;
+
+  console.log(type, reminderTime);
+  sendPushNotification(type, reminderTime);
   res.send("Sending push notification");
-  sendPushNotification(fcmMessage);
+});
+
+APP.get("/push-silent", (req, res) => {
+  sendSilentPushNotification(fcmMessage);
+  console.log(req);
+  res.send("Sending silent push notification");
 });
 
 APP.listen(PORT, () => {
